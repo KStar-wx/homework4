0048035f48f22d9ce7c4f19b7a44bd40c50f7bd1
KStar-wx <61170343+KStar-wx@users.noreply.github.com>
2020-03-16 21:58:49 -0500

    Add files via upload

---
+++ unit4/test/func.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a+b
+
+def minus(a, b):
+    return a-b
+
+def divide(a, b):
+    return a/b
+
+def distance(x1, y1, x2, y2):    
+        dis = ((x1-x2)**2+(y1-y2)**2)**0.5
+        return dis
